6cf4b41a465347b5a536f958bfb3413675e9f149
ywxt <ywxtcwh@qq.com>
2021-04-27 15:22:09 +0800

    去除换乘

--- utils/util.js
+++ utils/util.js
@@ -1,11 +1,11 @@
 import { direction } from "../libs/dir.js";
-function filter(array,cb, context){
+function filter(array, cb, context) {
   context = context || this;  //确定上下文，默认为this
 
   var len = array.length;  //数组的长度
   var r = [];  //最终将返回的结果数组
-  for(var i = 0; i < len; i++){
-    if(cb.call(context, array[i], i, array)){  //filter回调函数的三个参数：元素值，元素索引，原数组
+  for (var i = 0; i < len; i++) {
+    if (cb.call(context, array[i], i, array)) {  //filter回调函数的三个参数：元素值，元素索引，原数组
       r.push(array[i]);
     }
   }
@@ -41,46 +41,50 @@ function polyline(coors) {
 
 function getRoutes(result, mask) {
   console.log(result);
-  const routes =  result.map(route => {
+  const routes = filter(result.map(route => {
     // 公共交通
     if (route.mode === undefined) {
-      const step = filter(route.steps,step => step.mode === 'TRANSIT')[0];
+      const steps = filter(route.steps, step => step.mode === 'TRANSIT');
+      if (steps.length > 1) return;
+      const step = steps[0];
       let score;
       let pl;
       let duration;
       let fare;
-      let line =filter( step.lines,line => line.vehicle === 'BUS' ||  line.vehicle === 'SUBWAY')[0];
+      let lines = filter(step.lines, line => line.vehicle === 'BUS' || line.vehicle === 'SUBWAY');
+      if (lines.length > 1) return;
+      const line = lines[0];
       pl = polyline(line.polyline);
       duration = line.duration;
       fare = line.price;
       let way;
-      if(line.vehicle === 'BUS'){
-        if(fare === -1) fare = 2;
-        else fare/=100;
-        score = direction(pl,line.duration,'bus',mask,fare);
+      if (line.vehicle === 'BUS') {
+        if (fare === -1) fare = 2;
+        else fare /= 100;
+        score = direction(pl, line.duration, 'bus', mask, fare);
         way = '公交车';
-      }else if(line.vehicle === 'SUBWAY'){
-        if(fare==-1) {
-          console.log(2+Math.ceil((line.distance/1000-4)/4)*1);
-          if(line.distance<4000) {fare = 2;}
-          else if(line.distance<12000)  { fare = 2+Math.ceil((line.distance/1000-4)/4)*1;}
-          else if(line.distance<24000) { fare = 4+Math.ceil((line.distance/1000-12)/6)*1;}
-          else if(line.distance<40000) {fare = 6+Math.ceil((line.distance/1000-24)/8)*1;}
-          else if(line.distance<50000) {fare = 8+Math.ceil((line.distance/1000-40)/10)*1;}
-          else {9 + Math.round((line.distance/1000-50)/20)*1;}
-        }else {
-          fare /=100;
+      } else if (line.vehicle === 'SUBWAY') {
+        if (fare == -1) {
+          console.log(2 + Math.ceil((line.distance / 1000 - 4) / 4) * 1);
+          if (line.distance < 4000) { fare = 2; }
+          else if (line.distance < 12000) { fare = 2 + Math.ceil((line.distance / 1000 - 4) / 4) * 1; }
+          else if (line.distance < 24000) { fare = 4 + Math.ceil((line.distance / 1000 - 12) / 6) * 1; }
+          else if (line.distance < 40000) { fare = 6 + Math.ceil((line.distance / 1000 - 24) / 8) * 1; }
+          else if (line.distance < 50000) { fare = 8 + Math.ceil((line.distance / 1000 - 40) / 10) * 1; }
+          else { 9 + Math.round((line.distance / 1000 - 50) / 20) * 1; }
+        } else {
+          fare /= 100;
         }
-        
         way = '地铁';
+       
       }
-      
-      let type;let s;
-      if(line.vehicle === 'BUS'){type='bus'; s= 0.4765;}
-      else if(line.vehicle === 'SUBWAY'){type = 'subway'; s =0.9752;}
+
+      let type; let s;
+      if (line.vehicle === 'BUS') { type = 'bus'; s = 0.4765; }
+      else if (line.vehicle === 'SUBWAY') { type = 'subway'; s = 0.9752; }
       return {
-        score:0,
-        marks:  [pl[0], pl[pl.length - 1]],
+        score: 0,
+        marks: [pl[0], pl[pl.length - 1]],
         type: type,
         polylines: [
           {
@@ -89,7 +93,7 @@ function getRoutes(result, mask) {
             width: 8
           }
         ],
-        tags: [way,line.title,line.destination.title+'方向',line.geton.title+'站出发',line.getoff.title+'站下车'],
+        tags: [way, line.title, line.destination.title + '方向', line.geton.title + '站出发', line.getoff.title + '站下车'],
         timespan: duration,
         distance: line.distance,
         fare: fare,
@@ -121,7 +125,7 @@ function getRoutes(result, mask) {
     // 步行 已修改
     else if (route.mode === 'WALKING') {
       const pl = polyline(route.polyline);
-      let fare  = 0;
+      let fare = 0;
       return {
         type: 'walking',
         score: 0,
@@ -143,22 +147,22 @@ function getRoutes(result, mask) {
     // 骑行 已修改
     else if (route.mode === 'BICYCLING') {
       const pl = polyline(route.polyline);
-      let fare =Math.ceil((route.duration)/30)*1.5;
+      let fare = Math.ceil((route.duration) / 30) * 1.5;
       return {
         type: 'bicycling',
         score: 0,
         marks: [pl[0], pl[pl.length - 1]],
-        polylines:   [
+        polylines: [
           {
             points: pl,
             color: '#FFFF00DD',
             width: 4
           }
         ],
-        tags: [ '骑车'],
+        tags: ['骑车'],
         timespan: route.duration,
         distance: route.distance,
-        fare : fare,
+        fare: fare,
         s: 14.651
       };
     } else {
@@ -166,26 +170,33 @@ function getRoutes(result, mask) {
         score: 0
       };
     }
-  });
-  let _s = routes.reduce((a,b)=>a.s+b.s)/routes.length;
-  let _t = routes.reduce((a,b)=>a.timespan+b.timespan)/routes.length;
-  let _m = routes.reduce((a,b)=>a.fare+b.fare)/routes.length;
-  return routes.map(route=>{
-    route.score = direction(route.polyline,route.timespan,route.type,mask,route.fare,_s,_t,_m);
-    route.tags.push('S='+route.score);
+  }), (element) => element !== undefined);
+  let _s = routes.reduce((a, b) => a.s + b.s) / routes.length;
+  let _t = routes.reduce((a, b) => a.timespan + b.timespan) / routes.length;
+  let _m = routes.reduce((a, b) => a.fare + b.fare) / routes.length;
+  let maxScore = 0;
+  const retRoutes =  routes.map(route => {
+    route.score = direction(route.polyline, route.timespan, route.type, mask, route.fare, _s, _t, _m);
+    if (maxScore <= route.score) maxScore = route.score;
     return route;
-  }).sort((a,b)=>a.score>b.score?-1:1);
-  
+  }).sort((a, b) => a.score > b.score ? -1 : 1);
+  return retRoutes.map(route => {
+    route.score = route.score / maxScore;
+    route.tags.push('S=' + route.score);
+    return route;
+  });
+
+
 }
-async function getLines(qqmapsdk,start,dest,type) {
-  return new Promise((resolve,reject)=> {
+async function getLines(qqmapsdk, start, dest, type) {
+  return new Promise((resolve, reject) => {
     qqmapsdk.direction({
       mode: type,
       from: start,
       to: dest,
       success: res => resolve(res),
-      fail: err=>reject(err)
-  });
+      fail: err => reject(err)
+    });
   });
 }
 module.exports = {
